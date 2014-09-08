85fc39a01c2b89c71c55884d515d340c52be8b60
Jonathan Wight <jwight@mac.com>
2014-09-07 20:29:55 -0700

    Added demo for SVG

---
+++ Demos/SwiftGraphicsDemo/SwiftGraphicsDemo/DemoSVGViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DemoSVGViewController.swift
+//  SwiftGraphicsDemo
+//
+//  Created by Jonathan Wight on 9/7/14.
+//  Copyright (c) 2014 schwa.io. All rights reserved.
+//
+
+import Cocoa
+
+import SwiftGraphics
+
+class DemoSVGViewController: NSViewController {
+
+    var svgView:SVGView! { get { return self.view as SVGView } }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let URL = NSBundle.mainBundle().URLForResource("SwiftOutline", withExtension:"svg")
+        let XMLDocument = NSXMLDocument(contentsOfURL:URL!, options:0, error:nil)
+        
+        var parser = SVGParser()
+        let document = parser.parseDocument(XMLDocument)
+        svgView.document = document
+        
+        //let document = SwiftGraphics.SVGDocument()
+    }
+    
+}
