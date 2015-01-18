19b2f6332a419840477806043b45fd00c20d63cc
Jonathan Wight <jwight@mac.com>
2015-01-17 21:25:23 -0800

    Made GrahamScan playground match MonotoneChain

--- Playgrounds/GrahamScan.playground/section-1.swift
+++ Playgrounds/GrahamScan.playground/section-1.swift
@@ -1,34 +1,25 @@
 // Playground - noun: a place where people can play
 
 import Cocoa
-
 import SwiftGraphics
 import SwiftGraphicsPlayground
 import XCPlayground
 
-func SGPRender(identifier:String, block:((ctx:CGContext, bounds:CGRect) -> Void)) {
-    let demoView = SwiftGraphicsPlayground.DemoView(frame:CGRect(size:CGSize(w:480, h:320)))
-    demoView.drawBlock = block
-    XCPShowView(identifier, demoView)
-}
+var points = arrayOfRandomPoints(50, CGRect(w:480, h:320))
 
-var points = arrayOfRandomPoints(50, CGRect(w:400, h:300))
+let hull = grahamScan(points)
 
-SGPRender("Test") {
+SGPRender("Test", XCPShowView) {
     (ctx:CGContext, bounds:CGRect) in
-
-    let hull = grahamScan(points)
-
     for (index, point) in enumerate(points) {
-        if contains(hull, point) {
-            NSColor.greenColor().set()
-        }
-        else {
-            NSColor.redColor().set()
-        }
+        let color = contains(hull, point) ? NSColor.redColor() : NSColor.blackColor()
+        color.set()
+
         ctx.strokeCross(CGRect(center:point, radius:5))
         ctx.drawLabel("\(index)", point:point + CGPoint(x:2, y:0), size:10)
     }
-
+    
+    ctx.strokeLine(hull, close:true)
 }
+
 
