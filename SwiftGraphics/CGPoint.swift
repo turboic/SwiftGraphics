cd36a6577c6673646be942dbea3dbb8e8b980e20
Jonathan Wight <jwight@mac.com>
2015-01-18 21:54:52 -0800

    Clarified parameter names for angle.

--- SwiftGraphics/CGPoint.swift
+++ SwiftGraphics/CGPoint.swift
@@ -256,10 +256,10 @@ public func collinear(a:CGPoint, b:CGPoint, c:CGPoint) -> Bool {
     return (b.x - a.x) * (c.y - a.y) ==% (c.x - a.x) * (b.y - a.y)
 }
 
-public func angle(p0:CGPoint, p1:CGPoint, p2:CGPoint) -> CGFloat {
-    let x10 = p1.x - p0.x
-    let y10 = p1.y - p0.y
-    let x20 = p2.x - p0.x
-    let y20 = p2.y - p0.y
+public func angle(vertex:CGPoint, p1:CGPoint, p2:CGPoint) -> CGFloat {
+    let x10 = p1.x - vertex.x
+    let y10 = p1.y - vertex.y
+    let x20 = p2.x - vertex.x
+    let y20 = p2.y - vertex.y
     return atan2(abs(x10 * y20 - y10 * x20), x10 * x20 + y10 * y20);
     }
